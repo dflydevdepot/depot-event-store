ecf961bf882baf9275156753644fcb06b37e5bc0
Alec Charlton <alec.charlton@monii.com>
2015-07-24 10:29:46 +0100

    Additions to exception methods

--- src/Monii/AggregateEventStorage/Aggregate/Error/DeserializationNotPossible.php
+++ src/Monii/AggregateEventStorage/Aggregate/Error/DeserializationNotPossible.php
@@ -6,4 +6,30 @@ use InvalidArgumentException;
 
 class DeserializationNotPossible extends InvalidArgumentException
 {
+    /**
+     * @param mixed $object
+     *
+     * @return self
+     */
+    public static function becauseObjectHasAnUnexpectedType(
+        $object,
+        $expectedType
+    ) {
+        return new DeserializationNotPossible(sprintf(
+            'Aggregate object type "%s" is not an instance of "%s".',
+            get_class($object),
+            $expectedType
+        ));
+    }
+
+    public static function becauseClassDoesNotHaveAnExpectedStaticMethod(
+        $className,
+        $expectedStaticMethodName
+    ) {
+        return new self(sprintf(
+            'Aggregate class "%s" does not have a static method named "%s".',
+            $className,
+            $expectedStaticMethodName
+        ));
+    }
 }
--- src/Monii/AggregateEventStorage/Aggregate/Error/SerializationNotPossible.php
+++ src/Monii/AggregateEventStorage/Aggregate/Error/SerializationNotPossible.php
@@ -6,4 +6,30 @@ use InvalidArgumentException;
 
 class SerializationNotPossible extends InvalidArgumentException
 {
+    /**
+     * @param mixed $object
+     *
+     * @return self
+     */
+    public static function becauseObjectHasAnUnexpectedType(
+        $object,
+        $expectedType
+    ) {
+        return new SerializationNotPossible(sprintf(
+            'Aggregate object type "%s" is not an instance of "%s".',
+            get_class($object),
+            $expectedType
+        ));
+    }
+
+    public static function becauseClassDoesNotHaveAnExpectedStaticMethod(
+        $className,
+        $expectedStaticMethodName
+    ) {
+        return new self(sprintf(
+            'Aggregate class "%s" does not have a static method named "%s".',
+            $className,
+            $expectedStaticMethodName
+        ));
+    }
 }
