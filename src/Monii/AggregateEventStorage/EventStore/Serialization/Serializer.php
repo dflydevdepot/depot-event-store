a436fe067a31207bc16b4503138d9a1bb83dce6a
Alec Charlton <alec.charlton@monii.com>
2015-07-24 10:05:08 +0100

    Add canSerialize and canDeserialize methods

--- src/Monii/AggregateEventStorage/EventStore/Serialization/Serializer.php
+++ src/Monii/AggregateEventStorage/EventStore/Serialization/Serializer.php
@@ -10,9 +10,25 @@ interface Serializer
      * @param Contract $type
      * @param object $object
      *
+     * @return bool
+     */
+    public function canSerialize($type, $object);
+
+    /**
+     * @param Contract $type
+     * @param object $object
+     *
      * @return array
      */
     public function serialize($type, $object);
+
+    /**
+     * @param Contract $type
+     * @param array $data
+     *
+     * @return bool
+     */
+    public function canDeserialize($type, array $data);
 
     /**
      * @param Contract $type
