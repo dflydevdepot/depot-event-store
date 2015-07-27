4bab25642d2c2162bea0cc13c2df3c64d867c6dd
Steve Marks <steve.marks@monii.com>
2015-07-27 20:03:56 +0100

    Support for multiple data types, plus new isArray() method

--- src/Monii/AggregateEventStorage/EventStore/Serialization/Adapter/PropertiesReflection/ReflectionPropertyHelper.php
+++ src/Monii/AggregateEventStorage/EventStore/Serialization/Adapter/PropertiesReflection/ReflectionPropertyHelper.php
@@ -15,9 +15,19 @@ class ReflectionPropertyHelper
     private $reflectionProperty;
 
     /**
-     * @var string|null
+     * @var array|null
      */
-    private $type;
+    private $types;
+
+    /**
+     * @var boolean|null
+     */
+    private $isObject = false;
+
+    /**
+     * @var boolean
+     */
+    private $isArray = false;
 
     public function __construct(\ReflectionClass $reflectionClass, \ReflectionProperty $reflectionProperty)
     {
@@ -25,15 +35,25 @@ class ReflectionPropertyHelper
 
         if (preg_match('/@var\s+([^\s]+)/', $reflectionProperty->getDocComment(), $matches)) {
             list(, $type) = $matches;
-            if (class_exists($type)) {
-                $this->type = $type;
-            } else {
-                $type = $reflectionClass->getNamespaceName().'\\'.$type;
-                if (class_exists($type)) {
-                    $this->type = $type;
-                } else {
-                    $this->type = $type = null;
+            $types = explode("|", $type);
+            if (!empty($types)) {
+                foreach ($types as $type) {
+                    if (class_exists($type)) {
+                        // Object
+                        $this->types[] = $type;
+                        $this->isObject = $type;
+                    } else {
+                        $type = $reflectionClass->getNamespaceName() . '\\' . $type;
+                        if (class_exists($type)) {
+                            // Object
+                            $this->types[] = $type;
+                            $this->isObject = $type;
+                        } else {
+                            $this->types[] = $type = null;
+                        }
+                    }
                 }
+                array_unique($this->types);
             }
         }
 
@@ -43,12 +63,17 @@ class ReflectionPropertyHelper
 
     public function isObject()
     {
-        return ! is_null($this->type);
+        return ($this->isObject !== false);
+    }
+
+    public function isArray()
+    {
+        return in_array('array', $this->types);
     }
 
     public function getType()
     {
-        return $this->type;
+        return $this->isObject;
     }
 
     public function getValue($object)
