6d37bd61a3e9aedd57db15518cf9212fda7ddad8
Steve Marks <steve.marks@monii.com>
2015-07-27 15:27:56 +0100

    Correct return variable type declared in comment

--- src/Monii/AggregateEventStorage/EventStore/Serialization/Serializer.php
+++ src/Monii/AggregateEventStorage/EventStore/Serialization/Serializer.php
@@ -18,7 +18,7 @@ interface Serializer
      * @param Contract $type
      * @param object $object
      *
-     * @return object
+     * @return array
      */
     public function serialize(Contract $type, $object);
 
