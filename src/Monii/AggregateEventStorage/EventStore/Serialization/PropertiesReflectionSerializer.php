74d935a2b0181c703e8ec0c09100c997d0b1dfdf
Steve Marks <steve.marks@monii.com>
2015-07-27 14:19:47 +0100

    Complete methods for PropertiesReflectionSerializer

--- src/Monii/AggregateEventStorage/EventStore/Serialization/PropertiesReflectionSerializer.php
+++ src/Monii/AggregateEventStorage/EventStore/Serialization/PropertiesReflectionSerializer.php
@@ -2,36 +2,55 @@
 
 namespace Monii\AggregateEventStorage\EventStore\Serialization;
 
+use Monii\AggregateEventStorage\Contract\Contract;
+
 class PropertiesReflectionSerializer implements Serializer
 {
    /**
      * (@inheritdoc)
      */
-    public function canSerialize($type, $object)
+    public function canSerialize(Contract $type, $object)
     {
-
+        return true;
     }
 
     /**
      * (@inheritdoc)
      */
-    public function serialize($type, $object)
+    public function serialize(Contract $type, $object)
     {
+        $data = [];
+        $reflectionClass = new \ReflectionClass($object);
 
+        foreach ($reflectionClass->getProperties() as $reflectionProperty) {
+            $reflectionProperty->setAccessible(true);
+            $data[$reflectionProperty->getName()] = $reflectionProperty->getValue($object);
+        }
+
+        return $data;
     }
     /**
      * (@inheritdoc)
      */
-    public function canDeserialize($type, array $data)
+    public function canDeserialize(Contract $type, array $data)
     {
-
+        return true;
     }
 
     /**
      * (@inheritdoc)
      */
-    public function deserialize($type, array $data)
+    public function deserialize(Contract $type, array $data)
     {
+        $reflectionClass = new \ReflectionClass($type->getClassName());
 
+        $object = $reflectionClass->newInstanceWithoutConstructor();
+
+        foreach ($reflectionClass->getProperties() as $reflectionProperty) {
+            $reflectionProperty->setAccessible(true);
+            $reflectionProperty->setValue($object, $data[$reflectionProperty->getName()]);
+        }
+
+        return $object;
     }
 }
